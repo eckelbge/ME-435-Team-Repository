5bb046b54f1f41d4fd6b19c0bb1b1a39ebf4c2f7
Gus Eckelbarger <eckelbge@rose-hulman.edu>
2026-09-21 14:44:12 -0400

    t

---
+++ PySerial.py
@@ -0,0 +1,23 @@
+import serial
+import time
+
+print("Learning Pyserial")
+
+ser = serial.Serial("/dev/ttyACM0",19200,timeout=10)
+
+time.sleep(2.0) #Necessary sometimes
+
+ser.reset_output_buffer()
+message="RESET"
+print(message)
+message_bytes=(message+"\n").encode()
+print(message_bytes)
+
+ser.write(message_bytes)
+
+response_bytes=ser.readline()
+print(response_bytes)
+response=response_bytes.decode().strip()
+print(response)
+
+ser.close()
